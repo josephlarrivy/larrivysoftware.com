b9b34160bfc52afa93e8b2e653ead595c4f7a1dd
josephlarrivy <joseph.larrivy@gmail.com>
2023-10-15 16:57:17 -0500

    testing Netlify deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+// import Home from "./Components/Home";
+// import About from "./Components/About";
+// import Services from "./Components/Services";
+// import NavBar from "./Components/NavBar";
+// import Contact from "./Components/Contact";
+// import Footer from "./Components/Footer";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isMobile]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div id="page-main-container">
+        <BrowserRouter>
+          <div id="nav-outer-container">
+            {/* {<NavBar isMobile={isMobile} />} */}
+          </div>
+          <div id="page-container">
+            <h2>Larrivy Software</h2>
+            <p>joseph.larrivy@gmail.com</p>
+            <Routes>
+              {/* <Route exact path="/"
+                element={<Home isMobile={isMobile} />}
+              />
+              <Route exact path="/about"
+                element={<About />}
+              />
+              <Route exact path="/services"
+                element={<Services />}
+              />
+              <Route exact path="/contact"
+                element={<Contact />}
+              /> */}
+            </Routes>
+          </div>
+          <div id="footer-outer-container">
+            {/* {<Footer />} */}
+          </div>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
