bae84ea137fad2e1d5a6fb5f5720508567dfaa97
josephlarrivy <joseph.larrivy@gmail.com>
2023-10-15 18:15:28 -0500

    all of the basics are up and running

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
-// import Home from "./Components/Home";
+import Home from "./components/Home.jsx";
 // import About from "./Components/About";
 // import Services from "./Components/Services";
 // import NavBar from "./Components/NavBar";
-// import Contact from "./Components/Contact";
+import Contact from "./components/Contact.jsx";
 // import Footer from "./Components/Footer";
 import './App.css';
 
@@ -29,21 +29,20 @@ function App() {
             {/* {<NavBar isMobile={isMobile} />} */}
           </div>
           <div id="page-container">
-            <h2>Larrivy Software</h2>
-            <p>joseph.larrivy@gmail.com</p>
+            
             <Routes>
-              {/* <Route exact path="/"
-                element={<Home isMobile={isMobile} />}
+              <Route exact path="/"
+                element={<Home />}
               />
-              <Route exact path="/about"
+              {/*<Route exact path="/about"
                 element={<About />}
               />
               <Route exact path="/services"
                 element={<Services />}
-              />
+              /> */}
               <Route exact path="/contact"
                 element={<Contact />}
-              /> */}
+              />
             </Routes>
           </div>
           <div id="footer-outer-container">
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+
+import { Link } from 'react-router-dom';
+import '../styles/Home.css'
+
+const Home = () => {
+
+    return(
+        <>
+            <Link to="/contact"><div className="box"></div></Link>
+
+            <h2>Larrivy Software</h2>
+        </>
+         
+    )
+}
+
+export default Home;
