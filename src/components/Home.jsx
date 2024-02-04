4dbf9caf1004651c373bcff17e0c9ee82d8dbb3e
josephlarrivy <joseph.larrivy@gmail.com>
2024-02-04 10:36:29 -0600

    added a little more seo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 
     return(
         <>
-            <Link to="/contact"><div alt="joseph larrivy software duluth minnesota React React.js Node Express front en developer" className="box"></div></Link>
+            <Link to="/contact"><div alt="joseph larrivy software duluth minnesota React React.js Node Express front end developer joseph larrivy larrivy software" className="box"></div></Link>
 
             <h2 alt="joseph larrivy software duluth minnesota React React.js Node Express front en developer">Larrivy Software</h2>
         </>
